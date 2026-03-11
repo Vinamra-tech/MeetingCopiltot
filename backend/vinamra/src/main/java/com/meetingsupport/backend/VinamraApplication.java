5926d17082068377328b8eafa23f494ea38a8743
Vinamra-tech <vinamrat4@gmail.com>
2026-03-11 14:43:25 +0530

    yes

---
+++ backend/vinamra/src/main/java/com/meetingsupport/backend/VinamraApplication.java
@@ -0,0 +1,14 @@
+package com.meetingsupport.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class VinamraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinamraApplication.class, args);
+    }
+
+}
